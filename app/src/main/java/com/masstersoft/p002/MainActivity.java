bbfe070de589a21dfff806f04dc8daa683c4d3a1
Masster <andrei80586@gmail.com>
2015-07-29 15:35:55 +0300

    First commit

---
+++ app/src/main/java/com/masstersoft/p002/MainActivity.java
@@ -0,0 +1,13 @@
+package com.masstersoft.p002;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_layout);
+    }
+}
